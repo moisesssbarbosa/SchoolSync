75f2a28658d7f803501ea95aa435e039704a0a96
Moises <moisesssbarbosa80@gmail.com>
2026-06-30 20:00:35 -0300

    fix: criacao do construtor Frequencia e metodo mostrarFaltas

--- Frequencia.java
+++ Frequencia.java
@@ -1,3 +1,11 @@
 public class Frequencia {
     int faltas;
+
+    public Frequencia(int faltas) {
+        this.faltas = faltas;
+    }
+
+    public void mostrarFaltas() {
+        System.out.println("O aluno teve: " + faltas + " faltas.");
+    }
 }
